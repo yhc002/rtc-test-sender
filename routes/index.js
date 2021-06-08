843ce3fd868a84a7f750f636bae744b4f981e8c2
youngheon.cho <youngheon.cho@lge.com>
2021-06-08 18:46:33 +0900

    pug test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const cors = require('cors');
+
 var router = express.Router();
+
+router.use(cors());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

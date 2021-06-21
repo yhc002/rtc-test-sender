b0b5cd1305139283f90025671344ae6bcd775f35
youngheon.cho <youngheon.cho@lge.com>
2021-06-21 11:53:54 +0900

    remove unnecessary files

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 const cors = require('cors');
 
@@ -13,7 +12,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.use(cors());
 
 app.use(logger('dev'));
@@ -23,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
